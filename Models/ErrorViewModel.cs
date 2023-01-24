9af229467545ca8302c7d662fb2aabec6e6cd7ce
Guilherme <guilhermeagenor1012@gmail.com>
2023-01-24 15:14:23 -0300

    Criar model

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EstoqueMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
